e39c17d542b136750eb09541d8c91d3a2d8f2334
Lucas Videla <videla.lucas@gmail.com>
2013-09-13 11:02:53 -0300

    Heroku database config

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect('postgres://confy:confy@localhost/confy', :loggers => [logger])
-  when :production  then Sequel.connect(URI.parse(ENV['DATABASE_URL']),  :loggers => [logger])
+  when :production  then Sequel.connect(ENV['DATABASE_URL'],  :loggers => [logger])
   when :test        then Sequel.connect("sqlite:///" + Padrino.root('db', "confy_test.db"), :loggers => [logger])
 end
 
