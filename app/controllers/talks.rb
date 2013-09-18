5e7bc01a044602872a63b0705aae8f3a19a0eae8
Lucas Videla <videla.lucas@gmail.com>
2013-09-18 09:45:35 -0300

    Removed talk list (until implementation of pagination and search features)

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -2,12 +2,6 @@ Confy::App.controllers :talks do
 
   @title = 'talks - confy'
 
-  get :list do
-    render 'talks/list',
-      locals: { talks: Talk.sorted, title: @title },
-      :layout => 'layout'
-  end
-
   get ':year/:conference_slug/:talk_slug' do
     conference = Conference.where(:year => params[:year], :slug => params[:conference_slug]).first
     talk = Talk.where(:conference_id => conference.id, :slug => params[:talk_slug]).first
