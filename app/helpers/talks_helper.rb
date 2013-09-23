b9c5154a75930c59cb77d49aca3bd2dc6316dfe4
Lucas Videla <videla.lucas@gmail.com>
2013-09-23 11:55:56 -0300

    Added YouTube video support

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -4,11 +4,19 @@ require 'json'
 Confy::App.helpers do
 
   def video_iframe(video_url)
-    "http://player.vimeo.com/video/#{video_url.split('/').last}"
+    if video_url.include? 'vimeo' then
+      "//player.vimeo.com/video/#{video_url.split('/').last}"
+    else
+      "//www.youtube.com/embed/#{video_url.split('=').last}"
+    end
   end
 
   def video_thumbnail(video_url)
-    result = Net::HTTP.get(URI.parse("http://vimeo.com/api/v2/video/#{video_url.split('/').last}.json"))
-    JSON.parse(result)[0]["thumbnail_large"]
+    if video_url.include? 'vimeo' then
+      result = Net::HTTP.get(URI.parse("http://vimeo.com/api/v2/video/#{video_url.split('/').last}.json"))
+      JSON.parse(result)[0]["thumbnail_large"]
+    else
+      "//img.youtube.com/vi/#{video_url.split('=').last}/maxresdefault.jpg"
+    end
   end
 end
