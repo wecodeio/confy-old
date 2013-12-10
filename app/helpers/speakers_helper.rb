6260de8d3ecf0d8fb2e3e8f25711b36ba9db54d8
Lucas Videla <videla.lucas@gmail.com>
2013-12-10 15:02:01 -0300

    Removed backend-checking for existing speaker avatar. Added client check via vanilla javascript

--- app/helpers/speakers_helper.rb
+++ app/helpers/speakers_helper.rb
@@ -1,32 +1,7 @@
-require 'uri'
-require 'net/http'
-
 Confy::App.helpers do
 
   def speaker_image_url(twitter)
-    url = "http://confy-assets.wecode.io/speakers/#{twitter}.jpeg"
-
-    if exists?(url) then
-      url
-    else
-      "http://confy-assets.wecode.io/speakers/generic-speaker.png"
-    end
-
-  end
-
-  private
-
-  def exists?(image_url)
-    url = URI.parse(image_url)
-    Net::HTTP.start(url.host, url.port) do |http|
-      response = http.head(url.path)
-      case response
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        true
-      else
-        false
-      end
-    end
+    "http://confy-assets.wecode.io/speakers/#{twitter}.jpeg"
   end
 
 end
