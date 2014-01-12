9c71fee22e25ccd28ff76e91bf18c6094a804475
Lucas Videla <videla.lucas@gmail.com>
2014-01-12 19:37:39 -0300

    Better perfomance on talks retrieval. Saved one query :)

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -3,10 +3,10 @@ Confy::App.controllers :talks do
   @title = 'talks - confy'
 
   get ':year/:conference_slug/:talk_slug' do
-    conference = Conference.where(:year => params[:year], :slug => params[:conference_slug]).first
-    talk = Talk.where(:conference_id => conference.id, :slug => params[:talk_slug]).first
+    conference = Conference.where(:year => params[:year], :slug => params[:conference_slug])
+    talk = Talk.where(:conference => conference, :slug => params[:talk_slug]).first
     render 'talks/show',
-      locals: { talk: talk, conference: conference, title: "\"#{talk.title}\" by #{talk.speakers.map{|s| '@' + s[:twitter] }.join(' & ')}, at @#{talk.conference.twitter}" },
+      locals: { talk: talk, conference: talk.conference, title: "\"#{talk.title}\" by #{talk.speakers.map{|s| '@' + s[:twitter] }.join(' & ')}, at @#{talk.conference.twitter}" },
       :layout => 'layout'
   end
 
