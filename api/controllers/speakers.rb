a73cbacff6914e8aed4c8dbfdc2abd7bdfd214d0
Joaquín Vicente <joakin@gmail.com>
2015-06-02 10:54:06 -0300

    Add /speakers API

--- api/controllers/speakers.rb
+++ api/controllers/speakers.rb
@@ -1,6 +1,11 @@
 Confy::Api.controllers :speakers do
   include JsonHelper
 
+  get :index do
+    render_json :'speakers/index.json',
+      locals: { speakers: Speaker.all }
+  end
+
   get ":id" do
     if speaker = Speaker[params[:id]]
       render_json :'speakers/show.json',
